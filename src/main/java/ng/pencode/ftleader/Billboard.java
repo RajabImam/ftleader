66c31e7b2ebf819fcc8bd45a031da2cca350beeb
Rajab Imam <rajabimam@yahoo.com>
2022-10-26 10:39:44 +0200

    Initial Commit

---
+++ src/main/java/ng/pencode/ftleader/Billboard.java
@@ -0,0 +1,21 @@
+package ng.pencode.ftleader;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Billboard extends Remote {
+
+    /**
+     * Returns the billboard message
+     * @return
+     * @throws RemoteException
+     */
+    String getMessage() throws RemoteException;
+
+    /**
+     * Sets the billboard message
+     * @param message
+     * @throws RemoteException
+     */
+    void setMessage(String message) throws RemoteException;
+}
